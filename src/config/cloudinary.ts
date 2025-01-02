104e8ed6e6d2b5878c9d7a1303e53a42ccda78b4
GabrielLima2803 <gabriellima2803@gmail.com>
2025-01-02 17:56:49 -0300

    feat: Configurando o cloudinary

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,46 +1,11 @@
-import 'dotenv/config'
-import { v2 as cloudinary } from 'cloudinary';
+import 'dotenv/config';
+import cloudinary from 'cloudinary';
 
 (async function() {
-
-    console.log('Cloudinary Config:', {
-        cloud_name: process.env.CLOUD_NAME,
-        api_key: process.env.CLOUD_API_KEY,
-        api_secret: process.env.CLOUD_API_SECRET,
-      });
-
-      
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUD_NAME, 
-        api_key: process.env.CLOUD_API_KEY, 
-        api_secret: process.env.CLOUD_API_SECRET 
-    });
-    
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
+  
+  cloudinary.v2.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY, 
+    api_secret: process.env.CLOUD_API_SECRET 
+  });
 })();
